fdeecd70c3af8fa136ea164052f47667ce236aa3
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-05-16 22:53:00 -0300

    swagger added

--- controllers/users.js
+++ controllers/users.js
@@ -18,9 +18,65 @@ const getSingle = async (req, res) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(users[0]);
         })
+};
+
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    try {
+        const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+        if (response.acknowledged) {
+            res.status(201).json({ id: response.insertedId });
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while creating the user.');
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userId }, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    }
 }
+
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    try {
+        const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userId });
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: 'User not found.' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
 
 module.exports = {
     getAll,
     getSingle,
-}
+    createUser,
+    updateUser,
+    deleteUser,
+};
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,13 @@ const router = require('express').Router();
 const usersController = require('../controllers/users');
 
 router.get('/', usersController.getAll);
+
 router.get('/:id', usersController.getSingle);
+
+router.post('/', usersController.createUser);
+
+router.put('/:id', usersController.updateUser);
+
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
